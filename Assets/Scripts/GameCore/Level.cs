66e1bbabe6757325c7cae94174ac49d575298231
Yousef Borhani <yousef@joyixir.com>
2022-10-19 16:24:03 +0330

    Initialize tube on level

--- Assets/Scripts/GameCore/Level.cs
+++ Assets/Scripts/GameCore/Level.cs
@@ -9,12 +9,14 @@ namespace GameCore
         [SerializeField] private int levelNumber;
         [SerializeField] private int ballsCount;
         [SerializeField] private Cup cup;
+        [SerializeField] private Tube tube;
 
         public int LevelNumber => levelNumber;
 
         private void Start()
         {
             cup.Initialize(ballsCount);
+            tube.Initialize(ballsCount);
         }
     }
 }
