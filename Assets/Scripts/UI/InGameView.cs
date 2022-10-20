875092ae53047a23cc41e9ef3893fb2e6b54bf05
Yousef Borhani <yousef@joyixir.com>
2022-10-21 00:12:28 +0330

    Add slider to in game view

--- Assets/Scripts/UI/InGameView.cs
+++ Assets/Scripts/UI/InGameView.cs
@@ -7,10 +7,15 @@ namespace UI
     public class InGameView : View
     {
         [SerializeField] private TextMeshProUGUI ballsCountText;
+        [SerializeField] private Slider slider;
 
         public void SetBallsCountText(int currentCount, int wholeCount)
         {
             ballsCountText.text = currentCount + " / " + wholeCount;
+            if (wholeCount != 0)
+                slider.value = (float) currentCount / wholeCount;
+            else 
+                Debug.LogError("Divided by zero");
         }
     }
 }
